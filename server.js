5145be23587ca3d7d30afa9a704ccceb8d2e8070
Parth <parthbidari13@gmail.com>
2023-11-22 17:53:46 +0530

    Added mongpDB connection

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express'); // express variable requires the package from express
+
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+
 const app = express(); // Creates an express application
 const port = 3000; 
 
@@ -10,7 +15,19 @@ app.get('/',(req,res)=>{
 })
 // In callback function we have two parameters, request (req) & response (res)
 
+app.get('/route0',(req,res)=>{
+    res.send('Route 0 is running ')
+})
+
 app.listen(port,()=>{
     console.log("App listening to port "+port);
     
+})
+
+mongoose.connect('mongodb+srv://parth3_mongo:parthdb48@cluster1.sqvug29.mongodb.net/')
+.then(()=>{
+        console.log('DataBase Connected.');
+    }
+).catch((error)=>{
+    console.log(error);
 })
