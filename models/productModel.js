4fe0779115383d4890632948b60b0f57302001de
Parth <parthbidari13@gmail.com>
2023-11-22 18:37:46 +0530

    Added mongpDB model

---
+++ models/productModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+
+// Schema is a part of a model and it contains info about the tables & columns
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Enter product name"],
+    },
+    qunatity: {
+      type: Number,
+      required: [true, "Enter product quantity"],
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+    // timestamps helps us to keep a track of time when the code interacts with the DB
+  }
+);
+
+
+const productModel = mongoose.model("products",productSchema); 
+// This is the model we made to make a Collection in the mongoDB
+
+// Now we will export our Model (productModel)
+module.exports = productModel;
--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-const express = require('express'); // express variable requires the package from express
+const express = require("express"); // express variable requires the package from express
 
 // Using Node.js `require()`
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const app = express(); // Creates an express application
-const port = 3000; 
+const port = 3000;
 
 // Declaring routes, we can declare multiple routes on the application
 // app.METHOD(PATH, HANDLER)
 
-app.get('/',(req,res)=>{
-    res.send('Hello Node API');
-})
+app.get("/", (req, res) => {
+  res.send("Hello Node API");
+});
 // In callback function we have two parameters, request (req) & response (res)
 
-app.get('/route0',(req,res)=>{
-    res.send('Route 0 is running ')
-})
+app.get("/route0", (req, res) => {
+  res.send("Route 0 is running ");
+});
 
-app.listen(port,()=>{
-    console.log("App listening to port "+port);
-    
-})
+mongoose
+  .connect("mongodb+srv://parth3_mongo:parthdb48@cluster1.sqvug29.mongodb.net/")
+  .then(() => {
+    console.log("DataBase Connected.");
 
-mongoose.connect('mongodb+srv://parth3_mongo:parthdb48@cluster1.sqvug29.mongodb.net/')
-.then(()=>{
-        console.log('DataBase Connected.');
-    }
-).catch((error)=>{
+    app.listen(port, () => {
+      console.log("App listening to port " + port);
+    });
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
